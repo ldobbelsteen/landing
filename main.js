30b0f25b83824e03969146e45e577ac551515658
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2023-12-06 11:22:43 +0100

    Cleanup

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ import "./styles.css";
 const primaryColor = "#173b69";
 const secondaryColor = "#491456";
 const body = document.getElementsByTagName("body")[0];
-const openAnimationTime = 1000;
+const openAnimationTime = 1250;
 
 let start;
 const step = (stamp) => {
   if (start === undefined) start = stamp;
   const elapsed = stamp - start;
   const fraction = elapsed / openAnimationTime;
-  const angle = 360 * Math.pow(fraction, 3);
+  const angle = 360 * Math.pow(fraction, 2);
   body.style.background = `linear-gradient(${angle}deg, ${secondaryColor}, ${primaryColor})`;
   if (elapsed < openAnimationTime) {
     window.requestAnimationFrame(step);
